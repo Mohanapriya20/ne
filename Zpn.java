297a3e6fbbbf269272b0c303499f5d7331400131
Mohanapriya20 <mohanagesan97@gmail.com>
2017-06-08 22:40:58 +0530

    Create Zpn.java

---
+++ Zpn.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Zpn
+{
+ public static void main(String args[])
+{
+ Scanner s=new Scanner(System.in);
+ int a=s.nextInt();
+ if(a==0)
+{
+System.out.println("given num is zero");
+}
+else if(a>0)
+{
+System.out.println("given num is positive");
+}
+else
+{
+System.out.println("given num is negative");
+}
+}
+}
